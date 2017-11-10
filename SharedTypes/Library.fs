2f67d0a27dbc280cda096a80b817ea7364c39053
MobKerney <jason.kerney@hunterindustries.com>
2017-11-10 15:04:46 -0800

    started basic types

--- SharedTypes/Library.fs
+++ SharedTypes/Library.fs
@@ -1,5 +1,30 @@
-namespace SharedTypes
+namespace SolStone.SharedTypes
+open System
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+type FailureType =
+    | GeneralFailure of string
+    | ExpectationFailure of string
+    | ExceptionFailure of Exception
+    | Ignored of String
+    | StandardNotMet of String
+    | SetupFailure of FailureType
+
+type TestResult =
+    | Success
+    | Failure of FailureType
+
+//type TestContext =
+//    {
+//        ContainerPath              : string list;
+//        TestName                   : string;
+//        CanonicalizedName          : string;
+//        GoldStandardPath           : string;
+//        Reporters                  : (unit -> IApprovalFailureReporter) List;
+//    }
+
+type Test =
+    {
+        TestContainerPath : string list
+        TestName : string
+        TestFunction : unit -> TestResult
+    }
