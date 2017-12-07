f66624f32ad31b79598335d1d818086f545ee332
MobKerney <jason.kerney@hunterindustries.com>
2017-12-07 14:50:05 -0800

    Added test Project

---
+++ Core/Verification.Tests/VerificationTestProgram.fs
@@ -0,0 +1,9 @@
+﻿open SolStone.TestRunner.Default.Framework
+open SolStone.Core.SharedTypes
+open SolStone.Reporters.Console.Reporter
+open SolStone.TestBuilder.Scripting
+
+[<EntryPoint>]
+let main _argv =
+    printfn "Hello F#"
+    0
