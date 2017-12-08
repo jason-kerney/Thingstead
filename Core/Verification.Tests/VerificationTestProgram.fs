9c19c35610d323e849cd0d8b32b499ff58221c03
MobKerney <jason.kerney@hunterindustries.com>
2017-12-07 16:40:59 -0800

    First feature expectsToBe

--- Core/Verification.Tests/VerificationTestProgram.fs
+++ Core/Verification.Tests/VerificationTestProgram.fs
@@ -3,7 +3,40 @@ open SolStone.Core.SharedTypes
 open SolStone.Reporters.Console.Reporter
 open SolStone.TestBuilder.Scripting
 
+let expectsToBe = SolStone.Core.Verification.expectsToBe
+
 [<EntryPoint>]
 let main _argv =
-    printfn "Hello F#"
-    0
+    product "SolStone" (
+        suite "Verification" (
+            feature "expectsToBe" [
+                "should return Success if given \"5 |> expectsToBe 5\""
+                    |> testedWith (fun _ ->
+                        let result = 5 |> expectsToBe 5
+                        if result = Success then Success
+                        else 
+                            result 
+                            |> sprintf "Success <> %A"
+                            |> ExpectationFailure
+                            |> Failure
+                    )
+
+                "should return Failure if given \"\\\"Hello\" |> expectsToBe \\\"Bob\\\"\""
+                    |> testedWith (fun _ ->
+                        let result = "Hello" |> expectsToBe "Bob"
+                        if result = ("\"Hello\" expected to be \"Bob\"" |> ExpectationFailure |> Failure)
+                        then Success
+                        elif result = Success
+                        then  "\"Hello\" was stated as being \"Bob\"" |> ExpectationFailure |> Failure
+                        else
+                            result 
+                            |> sprintf "%A was not the expected failure" 
+                            |> ExpectationFailure 
+                            |> Failure
+                    )
+            ]
+        )
+    ) 
+    |> executer
+    |> report
+    |> getFailCount
--- Core/Verification/Verification.fs
+++ Core/Verification/Verification.fs
@@ -1,4 +1,8 @@
-namespace Verification
+namespace SolStone.Core
 
-type Verification() = 
-    member this.X = "F#"
+open SolStone.Core.SharedTypes
+
+module Verification =
+    let expectsToBe expected actual = 
+        if expected = actual then Success
+        else  expected |> sprintf "%A expected to be %A" actual |> ExpectationFailure |> Failure
