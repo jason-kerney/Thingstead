580696a64c3e9268580a4c27aaa49743b6ec038a
MobKerney <jason.kerney@hunterindustries.com>
2017-12-07 16:44:09 -0800

    minor clean up

--- Core/Verification/Verification.fs
+++ Core/Verification/Verification.fs
@@ -5,4 +5,8 @@ open SolStone.Core.SharedTypes
 module Verification =
     let expectsToBe expected actual = 
         if expected = actual then Success
-        else  expected |> sprintf "%A expected to be %A" actual |> ExpectationFailure |> Failure
+        else  
+            expected 
+            |> sprintf "%A expected to be %A" actual 
+            |> ExpectationFailure 
+            |> Failure
