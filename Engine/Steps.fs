f39510fb96f8ab97efc184a083520e0cc8bea26f
BAGUETTE\bagheer <bagheer@gmail.com>
2018-07-06 08:45:25 -0700

    Removed base step

--- Engine/Steps.fs
+++ Engine/Steps.fs
@@ -2,10 +2,4 @@ namespace Thingstead.Engine
 
 open Thingstead.Types
 
-module Steps = 
-    let baseStep =
-        {
-            BeforeStep = fun env _ -> Ok env
-            Executor = fun env testMethod -> testMethod env
-            AfterStep = fun _ _ -> Ok ()
-        }
+module Steps = ()
